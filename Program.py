fd2b6e678625d85c84bf7ea1810d58dbd218512f
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-12-01 15:28:38 -0800

    Pilot 0.2x
    Uploaded on Dec .1 2021 - v0

---
+++ Program.py
@@ -0,0 +1,35 @@
+###### this is sky pilot 0.2x 
+## it uses cohere.ai api
+## to do anything you want, compile cmake codes for any platform, hack into any device and more generate money for your company
+## this is a free software, you can use it for any purpose, but you can't sell it
+print("Welcome to Pilot 0.2x")
+import cohere # cohere.ai api
+## write a grad api prigvate key for cohere api usage
+## you can get one from https://cohere.ai/developers/
+import cohere
+import time
+##write a grad api prigvate key for cohere api usage
+cohere.key = "1HDueTvwSzVnHit5yRL5tM4H4xdVOM4OOz5Cr0um"
+# import time
+import os
+import sys
+import subprocess
+import json
+import requests
+import random
+import string
+import urllib.request
+import urllib.parse
+import urllib.error
+import urllib.request
+import urllib.error
+## import hacking tools
+import base64
+import hashlib
+import hmac
+import binascii
+import random
+import nmap
+def program(): {
+    print("Welcome to Pilot 0.2x")
+}
